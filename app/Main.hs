58ba387a5d1d25eb657b750a9ddc308ec6081b72
Michal Filipiuk <mkfilipiuk@gmail.com>
2019-03-29 19:00:15 +0100

    Add hindentation

--- app/Main.hs
+++ app/Main.hs
@@ -1,232 +1,287 @@
 -- JPP Haskell First Assignment
 -- Main module
 -- Michał Filipiuk (mf385423@students.mimuw.edu.pl)
-
 module Main where
 
-import Mon
-import Lib
+import Control.Monad.IO.Class
+import Control.Monad.State.Lazy
 import Data.Maybe
+import Lib
+import Mon
 import System.Environment
 import qualified Text.Read as Read
-import Control.Monad.State.Lazy
-import Control.Monad.IO.Class
 
 firstLine = "300 400 translate"
+
 lastLine = "stroke showpage"
+
 errorLine = "/Courier findfont 24 scalefont setfont 0 0 moveto (Error) show"
 
-data ProgramState = ProgramState { n :: Int
-                                 , stack :: [R]
-                                 , startPoint :: Point
-                                 , startPointIsDefined :: Bool
-                                 , currentPoint :: Point
-                                 , currentPointIsDefined :: Bool
-                                 , currentTransform :: Transform
-                                 , lengthOfCurrentPath :: Int
-                                 , picture :: Picture
-                                 , isError :: Bool
-                                 }
+add = "add"
 
-startState scale = ProgramState { n = scale
-                                , stack = []
-                                , startPoint = point (0,0)
-                                , startPointIsDefined = False
-                                , currentPoint = point (0,0)
-                                , currentPointIsDefined = False
-                                , currentTransform = TranformsList []
-                                , lengthOfCurrentPath = 0
-                                , picture = Picture {pictureLines = []}
-                                , isError = False
-                                }
+sub = "sub"
+
+divide = "div"
+
+mul = "mul"
+
+moveTo = "moveto"
+
+lineTo = "lineto"
+
+closePath = "closepath"
+
+translation = "translate"
+
+rotation = "rotate"
+
+data ProgramState = ProgramState
+  { n :: Int
+  , stack :: [R]
+  , startPoint :: Point
+  , startPointIsDefined :: Bool
+  , currentPoint :: Point
+  , currentPointIsDefined :: Bool
+  , currentTransform :: Transform
+  , lengthOfCurrentPath :: Int
+  , picture :: Picture
+  , isError :: Bool
+  }
+
+startState scale =
+  ProgramState
+    { n = scale
+    , stack = []
+    , startPoint = point (0, 0)
+    , startPointIsDefined = False
+    , currentPoint = point (0, 0)
+    , currentPointIsDefined = False
+    , currentTransform = TranformsList []
+    , lengthOfCurrentPath = 0
+    , picture = Picture {pictureLines = []}
+    , isError = False
+    }
 
 safeHead :: [a] -> Maybe a
-safeHead l = if null l then
-               Nothing
-             else
-               Just (head l)
+safeHead l =
+  if null l
+    then Nothing
+    else Just (head l)
 
 pop :: State ProgramState (Maybe R)
 pop = do
-        s <- get
-        let stackList = stack s
-        let a = safeHead stackList
-        if isJust a then do
-          put $ s {stack = tail stackList}
-          return a
-        else do
-          return a
+  s <- get
+  let stackList = stack s
+  let a = safeHead stackList
+  if isJust a
+    then do
+      put $ s {stack = tail stackList}
+      return a
+    else return a
 
 push :: R -> State ProgramState ()
 push i = do
-           s <- get
-           put $ s {stack = (i: stack s)}
-
-add = "add"
-sub = "sub"
-divide = "div"
-mul = "mul"
-moveTo = "moveto"
-lineTo = "lineto"
-closePath = "closepath"
-translation = "translate"
-rotation = "rotate"
+  s <- get
+  put $ s {stack = i : stack s}
 
 executeOperation :: (R -> R -> R) -> State ProgramState ()
 executeOperation op = do
-                        previousState <- get
-                        elem2 <- pop
-                        elem1 <- pop
-                        s <- get
-                        case (elem1, elem2) of
-                          (Just element1, Just element2) -> do put $ s {stack = ((op element1 element2) : stack s)}
-                          _ -> do put $ previousState {isError = True}
-
+  previousState <- get
+  elem2 <- pop
+  elem1 <- pop
+  s <- get
+  case (elem1, elem2) of
+    (Just element1, Just element2) ->
+      put $ s {stack = op element1 element2 : stack s}
+    _ -> put $ previousState {isError = True}
 
 executeAdd = executeOperation (+)
 
 executeSub = executeOperation (-)
 
 executeDiv = do
-               previousState <- get
-               elem2 <- pop
-               elem1 <- pop
-               s <- get
-               case (elem1, elem2) of
-                 (Just element1, Just element2) | (element2 /= 0) -> do (put $ s {stack = (element1 / element2 : stack s)})
-                 _ -> do put $ previousState {isError = True}
+  previousState <- get
+  elem2 <- pop
+  elem1 <- pop
+  s <- get
+  case (elem1, elem2) of
+    (Just element1, Just element2)
+      | element2 /= 0 -> put $ s {stack = element1 / element2 : stack s}
+    _ -> put $ previousState {isError = True}
 
 executeMul = executeOperation (*)
 
 executeMoveTo :: State ProgramState ()
 executeMoveTo = do
-                  previousState <- get
-                  elem2 <- pop
-                  elem1 <- pop
-                  s <- get
-                  case (elem1, elem2) of
-                    (Just element1, Just element2) -> do (put $ s {startPoint = transformedPoint
-                                                                  ,currentPoint = transformedPoint
-                                                                  ,startPointIsDefined = True
-                                                                  ,currentPointIsDefined = True
-                                                                  })
-                                                      where transformedPoint = trpoint (currentTransform s) (point (element1, element2))
-                    _ -> do put $ previousState {isError = True}
+  previousState <- get
+  elem2 <- pop
+  elem1 <- pop
+  s <- get
+  case (elem1, elem2) of
+    (Just element1, Just element2) ->
+      put $
+      s
+        { startPoint = transformedPoint
+        , currentPoint = transformedPoint
+        , startPointIsDefined = True
+        , currentPointIsDefined = True
+        }
+      where transformedPoint =
+              trpoint (currentTransform s) (point (element1, element2))
+    _ -> put $ previousState {isError = True}
 
 executeLineTo :: State ProgramState ()
 executeLineTo = do
-                  previousState <- get
-                  if currentPointIsDefined previousState == False then do
-                    put $ previousState {isError = True}
-                  else do
-                    elem2 <- pop
-                    elem1 <- pop
-                    s <- get
-                    case (elem1, elem2) of
-                      (Just element1, Just element2) -> do (put $ s {currentPoint = transformedPoint
-                                                                    ,picture = (line (coordinates $ currentPoint s) (coordinates transformedPoint)) & (picture s)
-                                                                    ,lengthOfCurrentPath = (lengthOfCurrentPath s) + 1
-                                                                    })
-                                                        where transformedPoint = trpoint (currentTransform s) (point (element1, element2))
-                      _ -> do put $ previousState {isError = True}
+  previousState <- get
+  if not (currentPointIsDefined previousState)
+    then put $ previousState {isError = True}
+    else do
+      elem2 <- pop
+      elem1 <- pop
+      s <- get
+      case (elem1, elem2) of
+        (Just element1, Just element2) ->
+          put $
+          s
+            { currentPoint = transformedPoint
+            , picture =
+                line
+                  (coordinates $ currentPoint s)
+                  (coordinates transformedPoint) &
+                picture s
+            , lengthOfCurrentPath = lengthOfCurrentPath s + 1
+            }
+          where transformedPoint =
+                  trpoint (currentTransform s) (point (element1, element2))
+        _ -> put $ previousState {isError = True}
 
 addLineIfCurrentPath :: State ProgramState ()
 addLineIfCurrentPath = do
-                         s <- get
-                         if (lengthOfCurrentPath s) > 0 then do
-                           put $ s {picture = (line (coordinates $ currentPoint s)  (coordinates $ startPoint s)) & (picture s)
-                                   ,lengthOfCurrentPath = 0
-                                   }
-                         else do
-                           return ()
+  s <- get
+  if lengthOfCurrentPath s > 0
+    then put $
+         s
+           { picture =
+               line (coordinates $ currentPoint s) (coordinates $ startPoint s) &
+               picture s
+           , lengthOfCurrentPath = 0
+           }
+    else return ()
 
 executeClosePath :: State ProgramState ()
 executeClosePath = do
-                     addLineIfCurrentPath
-                     s <- get
-                     put $ s {picture = (transform (currentTransform s) $ picture s) & (picture s)
-                             ,currentPoint = startPoint s
-                             ,currentTransform = TranformsList []}
+  addLineIfCurrentPath
+  s <- get
+  put $
+    s
+      { picture = transform (currentTransform s) (picture s) & picture s
+      , currentPoint = startPoint s
+      , currentTransform = TranformsList []
+      }
 
 executeTranslate :: State ProgramState ()
 executeTranslate = do
-                     previousState <- get
-                     elem2 <- pop
-                     elem1 <- pop
-                     s <- get
-                     case (elem1, elem2) of
-                       (Just element1, Just element2) -> do (put $ s {currentTransform = (translate (Vec (Point (element1, element2)))) >< currentTransform s})
-                       _ -> do put $ previousState {isError = True}
-
+  previousState <- get
+  elem2 <- pop
+  elem1 <- pop
+  s <- get
+  case (elem1, elem2) of
+    (Just element1, Just element2) ->
+      put $
+      s
+        { currentTransform =
+            translate (Vec (Point (element1, element2))) >< currentTransform s
+        }
+    _ -> put $ previousState {isError = True}
 
 executeRotate :: State ProgramState ()
 executeRotate = do
-                  previousState <- get
-                  elem1 <- pop
-                  s <- get
-                  case elem1 of
-                    Just element1 -> do (put $ s {currentTransform = (rotate element1) >< currentTransform s})
-                    _ -> do put $ previousState {isError = True}
+  previousState <- get
+  elem1 <- pop
+  s <- get
+  case elem1 of
+    Just element1 ->
+      put $ s {currentTransform = rotate element1 >< currentTransform s}
+    _ -> put $ previousState {isError = True}
 
 setError :: State ProgramState ()
 setError = do
-             s <- get
-             put $ s {isError = True}
+  s <- get
+  put $ s {isError = True}
 
 parseInput :: [String] -> State ProgramState (Either String Picture)
-parseInput [] = do
-                  s <- get
-                  return (Right (picture s))
+parseInput [] = Right . picture <$> get
 parseInput (instruction:rest) = do
-                                  let integerNumber = (Read.readMaybe instruction :: Maybe Int)
-                                  case integerNumber of
-                                    Just nn -> do push (toRational nn)
-                                    _ -> (case () of
-                                           () | instruction == add -> executeAdd
-                                           () | instruction == sub -> executeSub
-                                           () | instruction == divide -> executeDiv
-                                           () | instruction == mul -> executeMul
-                                           () | instruction == moveTo -> executeMoveTo
-                                           () | instruction == lineTo -> executeLineTo
-                                           () | instruction == closePath -> executeClosePath
-                                           () | instruction == translation -> executeTranslate
-                                           () | instruction == rotation -> executeRotate
-                                           _ -> setError)
-                                  s <- get
-                                  if isError s then do
-                                    return (Left "Error")
-                                  else do
-                                    parseInput rest
+  let integerNumber = Read.readMaybe instruction :: Maybe Int
+  case integerNumber of
+    Just nn -> push (toRational nn)
+    _ ->
+      case () of
+        ()
+          | instruction == add -> executeAdd
+        ()
+          | instruction == sub -> executeSub
+        ()
+          | instruction == divide -> executeDiv
+        ()
+          | instruction == mul -> executeMul
+        ()
+          | instruction == moveTo -> executeMoveTo
+        ()
+          | instruction == lineTo -> executeLineTo
+        ()
+          | instruction == closePath -> executeClosePath
+        ()
+          | instruction == translation -> executeTranslate
+        ()
+          | instruction == rotation -> executeRotate
+        _ -> setError
+  s <- get
+  if isError s
+    then return (Left "Error")
+    else parseInput rest
 
 buildingRender :: String -> State ProgramState (Either String String)
 buildingRender standardInput = do
-                                 s <- get
-                                 pictureEither <- parseInput $ words standardInput
-                                 case pictureEither of
-                                   Right picture -> return (Right (printPicture (renderScaled (n s) (reversePicture picture))))
-                                   Left err -> return (Left err)
+  s <- get
+  pictureEither <- parseInput $ words standardInput
+  case pictureEither of
+    Right picture ->
+      return
+        (Right (printPicture (renderScaled (n s) (reversePicture picture))))
+    Left err -> return (Left err)
 
 auxPrintPicture :: IntRendering -> [String] -> [String]
 auxPrintPicture [] acc = acc
-auxPrintPicture (((x1,y1), (x2,y2)):ls) acc = auxPrintPicture ls ((show x1 ++ " " ++ show y1 ++ " " ++ moveTo ++ " " ++ show x2 ++ " " ++ show y2 ++ " " ++ lineTo ++ "\n"):acc)
+auxPrintPicture (((x1, y1), (x2, y2)):ls) acc =
+  auxPrintPicture
+    ls
+    ((show x1 ++
+      " " ++
+      show y1 ++
+      " " ++ moveTo ++ " " ++ show x2 ++ " " ++ show y2 ++ " " ++ lineTo ++ "\n") :
+     acc)
 
 printPicture :: IntRendering -> String
 printPicture intRendering = concat $ reverse $ auxPrintPicture intRendering []
 
 processInput :: Int -> String -> String
-processInput scale standardInput = case evalState (buildingRender standardInput) (startState scale) of
-                                     Left _ -> errorLine
-                                     Right s -> s
-
+processInput scale standardInput =
+  case evalState (buildingRender standardInput) (startState scale) of
+    Left _ -> errorLine
+    Right s -> s
 
 main :: IO ()
 main = do
-         args <- getArgs
-         let argsWithOne = if null args then ["1"] else args
-         let scale = Read.readMaybe $ head argsWithOne
-         case scale of
-           Just n -> do
-                       standardInput <- getContents
-                       let commandsToExecute = processInput n standardInput
-                       putStrLn (firstLine ++ "\n\n" ++ commandsToExecute ++ "\n\n" ++ lastLine)
-           _ -> putStrLn "Usage: ./program scale"
+  args <- getArgs
+  let argsWithOne =
+        if null args
+          then ["1"]
+          else args
+  let scale = Read.readMaybe $ head argsWithOne
+  case scale of
+    Just n -> do
+      standardInput <- getContents
+      let commandsToExecute = processInput n standardInput
+      putStrLn (firstLine ++ "\n\n" ++ commandsToExecute ++ "\n\n" ++ lastLine)
+    _ -> putStrLn "Usage: ./program scale"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,6 @@
 -- JPP Haskell First Assignment
 -- Lib module
 -- Michał Filipiuk (mf385423@students.mimuw.edu.pl)a
-
 module Lib where
 
 import Data.Ratio
@@ -9,112 +8,143 @@ import Mon
 
 -- Types and useful constants
 type R = Rational
-type R2 = (R,R)
+
+type R2 = (R, R)
 
 zeroR = 0 % 1
+
 zeroR2 = (zeroR, zeroR)
 
 -- Point declaration and implementation
-newtype Point = Point {coordinates :: R2}
+newtype Point = Point
+  { coordinates :: R2
+  }
 
 instance Eq Point where
-  Point {coordinates = (a,b)} == Point {coordinates = (x,y)} = a == x && b == y
+  Point {coordinates = (a, b)} == Point {coordinates = (x, y)} =
+    a == x && b == y
 
 instance Show Point where
-  show Point {coordinates = (a,b)} = "(" ++ show a ++ " , " ++ show b ++ ")"
+  show Point {coordinates = (a, b)} = "(" ++ show a ++ " , " ++ show b ++ ")"
 
 point :: R2 -> Point
 point r2 = Point {coordinates = r2}
 
 pointZero = Point {coordinates = zeroR2}
 
-
 -- Vec declaration and implementation
-newtype Vec = Vec { direction :: Point }
+newtype Vec = Vec
+  { direction :: Point
+  }
 
 instance Eq Vec where
   Vec {direction = a} == Vec {direction = x} = a == x
 
 instance Show Vec where
-  show Vec {direction = a} = concat ["(", show a , ")"]
+  show Vec {direction = a} = concat ["(", show a, ")"]
 
 instance Mon Vec where
   m1 = Vec {direction = pointZero}
-  (><) Vec {direction = Point {coordinates = (a,b)}} Vec {direction = Point {coordinates = (x,y)}} = Vec {direction = Point {coordinates = (a+x,b+y)}}
+  (><) Vec {direction = Point {coordinates = (a, b)}} Vec {direction = Point {coordinates = (x, y)}} =
+    Vec {direction = Point {coordinates = (a + x, b + y)}}
 
 vec :: R2 -> Vec
-vec r2 = Vec { direction = point r2 }
+vec r2 = Vec {direction = point r2}
 
 -- Picture declaration, implementation and associated types
 type Line = (Point, Point)
 
-newtype Picture = Picture {pictureLines :: [Line]}
+newtype Picture = Picture
+  { pictureLines :: [Line]
+  }
 
-line :: (R,R) -> (R,R) -> Picture
+line :: (R, R) -> (R, R) -> Picture
 line rs1 rs2 = Picture {pictureLines = [(point rs1, point rs2)]}
 
 rectangle :: R -> R -> Picture
-rectangle r1 r2 = Picture {pictureLines = [( point zeroR2, point (zeroR, r1))
-                                          ,( point zeroR2, point (zeroR, r2))
-                                          ,( point (zeroR, r1), point (r1,r2))
-                                          ,( point (zeroR, r2), point (r1,r2))
-                                          ]}
+rectangle r1 r2 =
+  Picture
+    { pictureLines =
+        [ (point zeroR2, point (zeroR, r1))
+        , (point zeroR2, point (zeroR, r2))
+        , (point (zeroR, r1), point (r1, r2))
+        , (point (zeroR, r2), point (r1, r2))
+        ]
+    }
 
 reversePicture Picture {pictureLines = l} = Picture {pictureLines = reverse l}
 
 lenghtOfPicture Picture {pictureLines = l} = length l
 
 (&) :: Picture -> Picture -> Picture
-(&) Picture {pictureLines = l1} Picture {pictureLines = l2} = Picture {pictureLines = l1 ++ l2}
-
+(&) Picture {pictureLines = l1} Picture {pictureLines = l2} =
+  Picture {pictureLines = l1 ++ l2}
 
 -- Integer rendering types and methods
-type IntLine = ((Int,Int), (Int,Int))
+type IntLine = ((Int, Int), (Int, Int))
+
 type IntRendering = [IntLine]
 
 renderScaled :: Int -> Picture -> IntRendering
 renderScaled i Picture {pictureLines = l} = map (scaleLine $ toInteger i) l
 
 scaleLine :: Integer -> Line -> IntLine
-scaleLine i (p1, p2) = (roundPoint (scalePoint i p1), roundPoint (scalePoint i p2))
+scaleLine i (p1, p2) =
+  (roundPoint (scalePoint i p1), roundPoint (scalePoint i p2))
 
 scalePoint :: Integer -> Point -> Point
-scalePoint i Point {coordinates = (a,b)} = Point {coordinates = ((i % 1)*a, (i % 1)*b)}
+scalePoint i Point {coordinates = (a, b)} =
+  Point {coordinates = ((i % 1) * a, (i % 1) * b)}
 
-roundPoint :: Point -> (Int,Int)
-roundPoint Point {coordinates = (a,b)} = (round a, round b)
+roundPoint :: Point -> (Int, Int)
+roundPoint Point {coordinates = (a, b)} = (round a, round b)
 
 -- Transformations
-data AuxTransform =  VectorTransform Vec | RotationTransform R deriving (Eq, Show)
+data AuxTransform
+  = VectorTransform Vec
+  | RotationTransform R
+  deriving (Eq, Show)
 
 bhaskaraSineApproximation :: R -> R
-bhaskaraSineApproximation x = (4*x*(180 - x)) / (40500 - x*(180 - x))
+bhaskaraSineApproximation x = (4 * x * (180 - x)) / (40500 - x * (180 - x))
 
 rotateSine :: R -> R
-rotateSine d = if d < (180 % 1) then
-                 bhaskaraSineApproximation d
-               else
-                 -(bhaskaraSineApproximation (d-180))
+rotateSine d =
+  if d < (180 % 1)
+    then bhaskaraSineApproximation d
+    else -(bhaskaraSineApproximation (d - 180))
 
 rotateCosine :: R -> R
-rotateCosine d = rotateSine (normalizeDegree (d+90))
+rotateCosine d = rotateSine (normalizeDegree (d + 90))
 
 applyAuxTransformToPoint :: AuxTransform -> Point -> Point
-applyAuxTransformToPoint (VectorTransform translation) p = direction (Vec { direction = p } >< translation)
-applyAuxTransformToPoint (RotationTransform rotation) Point {coordinates = (r1,r2)} = Point {coordinates = (r1*c - r2*s, r1*s + r2*c)}
-                                                                    where s = rotateSine rotation
-                                                                          c = rotateCosine rotation
+applyAuxTransformToPoint (VectorTransform translation) p =
+  direction (Vec {direction = p} >< translation)
+applyAuxTransformToPoint (RotationTransform rotation) Point {coordinates = (r1, r2)} =
+  Point {coordinates = (r1 * c - r2 * s, r1 * s + r2 * c)}
+  where
+    s = rotateSine rotation
+    c = rotateCosine rotation
 
-newtype Transform = TranformsList [AuxTransform] deriving (Eq, Show)
+newtype Transform =
+  TranformsList [AuxTransform]
+  deriving (Eq, Show)
 
 translate :: Vec -> Transform
 translate v = TranformsList [VectorTransform v]
 
 normalizeDegree :: R -> R
-normalizeDegree r = if r > 0 then
-                        r - (quot (numerator r) (denominator r* fullCircleInDegrees)*fullCircleInDegrees % 1)
-                      else
-                        r - ((quot (numerator r) (denominator r* fullCircleInDegrees)*fullCircleInDegrees - fullCircleInDegrees )% 1)
+normalizeDegree r =
+  if r > 0
+    then r -
+         (quot (numerator r) (denominator r * fullCircleInDegrees) *
+          fullCircleInDegrees %
+          1)
+    else r -
+         ((quot (numerator r) (denominator r * fullCircleInDegrees) *
+           fullCircleInDegrees -
+           fullCircleInDegrees) %
+          1)
 
 rotate :: R -> Transform
 rotate r = TranformsList [RotationTransform (normalizeDegree r)]
@@ -132,25 +162,43 @@ instance Mon Transform where
 mergeTransformations :: Transform -> Transform -> Transform
 mergeTransformations (TranformsList []) t2 = t2
 mergeTransformations t1 (TranformsList []) = t1
-mergeTransformations (TranformsList t1) (TranformsList t2) = auxMergeTransformations (TranformsList (reverse (t1 ++ t2))) []
+mergeTransformations (TranformsList t1) (TranformsList t2) =
+  auxMergeTransformations (TranformsList (reverse (t1 ++ t2))) []
 
 auxMergeTransformations :: Transform -> [AuxTransform] -> Transform
 auxMergeTransformations (TranformsList []) acc = TranformsList acc
-auxMergeTransformations (TranformsList (t:ts)) [] = auxMergeTransformations (TranformsList ts) [t]
-auxMergeTransformations (TranformsList (t:ts)) (acc:accs) = case (t,acc) of
-                                                              (VectorTransform v1, VectorTransform v2) -> auxMergeTransformations (TranformsList ts) (VectorTransform (v1 >< v2):accs)
-                                                              (RotationTransform r1, RotationTransform r2) -> auxMergeTransformations (TranformsList ts) (RotationTransform (normalizeDegree (r1 + r2)):accs)
-                                                              (VectorTransform v1, RotationTransform r2) -> auxMergeTransformations (TranformsList ts) ([VectorTransform v1, RotationTransform r2] ++ accs)
-                                                              (RotationTransform r1, VectorTransform v2) -> auxMergeTransformations (TranformsList ts) ([RotationTransform r1, VectorTransform v2] ++ accs)
+auxMergeTransformations (TranformsList (t:ts)) [] =
+  auxMergeTransformations (TranformsList ts) [t]
+auxMergeTransformations (TranformsList (t:ts)) (acc:accs) =
+  case (t, acc) of
+    (VectorTransform v1, VectorTransform v2) ->
+      auxMergeTransformations
+        (TranformsList ts)
+        (VectorTransform (v1 >< v2) : accs)
+    (RotationTransform r1, RotationTransform r2) ->
+      auxMergeTransformations
+        (TranformsList ts)
+        (RotationTransform (normalizeDegree (r1 + r2)) : accs)
+    (VectorTransform v1, RotationTransform r2) ->
+      auxMergeTransformations
+        (TranformsList ts)
+        ([VectorTransform v1, RotationTransform r2] ++ accs)
+    (RotationTransform r1, VectorTransform v2) ->
+      auxMergeTransformations
+        (TranformsList ts)
+        ([RotationTransform r1, VectorTransform v2] ++ accs)
 
 trpoint :: Transform -> Point -> Point
-trpoint (TranformsList auxTransforms) p = foldl (flip applyAuxTransformToPoint) p auxTransforms
+trpoint (TranformsList auxTransforms) p =
+  foldl (flip applyAuxTransformToPoint) p auxTransforms
 
 trvec :: Transform -> Vec -> Vec
-trvec (TranformsList auxTransforms) Vec { direction = p} = Vec { direction = foldl (flip applyAuxTransformToPoint) p auxTransforms}
+trvec (TranformsList auxTransforms) Vec {direction = p} =
+  Vec {direction = foldl (flip applyAuxTransformToPoint) p auxTransforms}
 
 trline :: Transform -> Line -> Line
-trline t (p1,p2) = (trpoint t p1, trpoint t p2)
+trline t (p1, p2) = (trpoint t p1, trpoint t p2)
 
 transform :: Transform -> Picture -> Picture
-transform t Picture {pictureLines = pl} = Picture {pictureLines = map (trline t) pl}
+transform t Picture {pictureLines = pl} =
+  Picture {pictureLines = map (trline t) pl}
